f7643fcd88e0d7246c86e5acc34cfad5c832e7a2
Sudeep Agrawal <Sudeep.Agrawal@gmail.com>
2016-12-26 18:22:48 -0500

    Initial code checkin.

---
+++ VirtualTourist/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VirtualTourist
+//
+//  Created by Sudeep Agrawal on 12/26/16.
+//  Copyright © 2016 Agrawal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

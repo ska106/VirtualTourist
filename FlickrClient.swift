1a4cc5e142ce4548b3ccf0935d3ddf5e543882fc
Sudeep Agrawal <Sudeep.Agrawal@gmail.com>
2016-12-28 22:35:44 -0500

    Completed Search API - Flickr

--- FlickrClient.swift
+++ FlickrClient.swift
@@ -15,16 +15,40 @@ class FlickrClient
     // MARK: Singleton Pattern
     static let sharedInstance = FlickrClient()
     
-    // MARK : Prepare the HTTP header for the request.
-    func setHeaders(request: NSMutableURLRequest) -> NSMutableURLRequest
+    // MARK : Based on the name of the resource, construct the API URL to be invoked.
+    func getMethodURL(using parameters: [String:AnyObject]) -> URL
     {
-        request.addValue(HeaderValues.JSON, forHTTPHeaderField: HeaderKeys.Accept)
-        request.addValue(HeaderValues.JSON, forHTTPHeaderField: HeaderKeys.ContentType)
-        return request
+        //Ref: www.grokswift.com/building-urls
+        var components = URLComponents()
+        components.scheme = Constants.APIScheme
+        components.host = Constants.APIHost
+        components.path = Constants.APIPath
+        components.queryItems = [URLQueryItem]()
+        
+        for (key, value) in parameters
+        {
+            let queryItem = URLQueryItem(name: key, value: "\(value)")
+            components.queryItems!.append(queryItem)
+        }
+        return components.url!
     }
 
+    private func makeBoundaryBoxString(lat latitude: Double,  long longitude: Double) -> String
+    {
+        //Longitude - limits
+        let minimumLon = max(longitude - Constants.BBoxLimits.Width, Constants.BBoxLimits.LonRange.0)
+        let maximumLon = min(longitude + Constants.BBoxLimits.Width, Constants.BBoxLimits.LonRange.0)
+        
+        //Latitude - imits
+        let minimumLat = max(latitude - Constants.BBoxLimits.Height, Constants.BBoxLimits.LatRange.0)
+        let maximumLat = min(latitude + Constants.BBoxLimits.Height, Constants.BBoxLimits.LatRange.0)
+        
+        //Ref: API Specifications on bbox @ https://www.flickr.com/services/api/flickr.photos.search.html
+        return "\(minimumLon),\(minimumLat),\(maximumLon),\(maximumLat)"
+    }
+    
     // MARK : Function to initiate the API call via. Task.
-    func makeTaskCall (request:URLRequest , completionHandlerForTaskCall : @escaping (_ result : AnyObject? , _ error: NSError?) -> Void)
+    func makeTaskCall (req request:URLRequest , completionHandlerForTaskCall : @escaping (_ result : AnyObject? , _ error: NSError?) -> Void)
     {
         let session = URLSession.shared
         
@@ -47,13 +71,63 @@ class FlickrClient
         task.resume()
     }
     
-    //Search Photos
-    func searchPhotos()
+    //Search Photos and get the photo URLs of all the photos in the search result.
+    func searchPhotos(latitude: Double, longitude: Double, completionHandlerForSearchPhotos: @escaping (_ photoURLS: [String]?, _ error: NSError?) -> Void)
     {
+        //Set the URL parameters as per the Flickr specifications
+        let methodParameters: [String: AnyObject] =
+        [
+            Request.ParamKey.Method:Request.ParamVal.Method as AnyObject,
+            Request.ParamKey.APIKey:Request.ParamVal.APIKey as AnyObject,
+            Request.ParamKey.BoundingBox:self.makeBoundaryBoxString(lat: latitude, long: longitude) as AnyObject,
+            Request.ParamKey.Extras:Request.ParamVal.MediumURL as AnyObject,
+            Request.ParamKey.Format:Request.ParamVal.Format as AnyObject,
+            Request.ParamKey.NoJSONCallback:Request.ParamVal.DisableJSONCallback as AnyObject,
+            Request.ParamKey.PerPage:Request.ParamVal.PhotosPerPage as AnyObject,
+            Request.ParamKey.Page:"\(arc4random_uniform(50))" as AnyObject
+        ]
+
+        let requestURL = URLRequest(url: getMethodURL(using: methodParameters));
         
-    }
-    //DownloadPhotos
-    func downloadPhotos()
-    {
+        makeTaskCall(req: requestURL){ (data, error) in
+        
+            if error == nil
+            {
+                //Success - No error in response
+                //Check the reponse for OK (stat).
+                guard let stat = data?[Response.Key.Status] as? String, stat == "OK" else
+                {
+                    print("Flickr API returned an error. See error code and message in \(data)")
+                    completionHandlerForSearchPhotos(nil, NSError(domain: Error.Domain.searchMethod, code: 5001, userInfo: [NSLocalizedDescriptionKey:Error.Message.Error_Occurred]))
+                    return
+                }
+                
+                //Parse through the response that contains elements photos/photo.
+                if let photosDictionary = data?[Response.Key.PhotosDictionary] as? [String:AnyObject],
+                   let photosArray = photosDictionary[Response.Key.Photo] as? [[String:AnyObject]]
+                {
+                    var photoURLS = [String]()
+                    
+                    for photo in photosArray
+                    {
+                        //Get the URLs of the photos.
+                        if let photoURL = photo[Response.Key.MediumURL] as? String
+                        {
+                            photoURLS.append(photoURL)
+                        }
+                    }
+                    completionHandlerForSearchPhotos(photoURLS, nil)
+                }
+                else
+                {
+                    completionHandlerForSearchPhotos(nil, NSError(domain: Error.Domain.searchMethod, code: 5002, userInfo: [NSLocalizedDescriptionKey: Error.Message.Invalid_Response]))
+                }
+            }
+            else
+            {
+                //Failure in invoking Flickr API.
+                completionHandlerForSearchPhotos(nil,error)
+            }
+        }
     }
 }
--- FlickrConstants.swift
+++ FlickrConstants.swift
@@ -4,65 +4,83 @@
 //
 //  Created by Sudeep Agrawal on 12/27/16.
 //  Copyright © 2016 Agrawal. All rights reserved.
-//
 
 import Foundation
 
+// Ref: https://www.flickr.com/services/api/flickr.photos.search.html
 extension FlickrClient
 {
     struct Constants
     {
-        static let ApiScheme = "https"
-        static let ApiHost = "api.flickr.com"
-        static let ApiPath = "/services/rest/"
-    }
-    
-    struct Methods
-    {
-        static let SearchPhotos = "flickr.photos.search"
-    }
-    
-    struct HeaderKeys
-    {
-        static let Accept = "Accept"
-        static let ContentType = "Content-Type"
-    }
-    
-    struct HeaderValues
-    {
-        static let JSON = "application/json"
+        //Ref: www.grokswift.com/building-urls
+        static let APIScheme = "https"
+        static let APIHost = "api.flickr.com"
+        static let APIPath = "/services/rest"
+        
+        
+        static let PhotoLimit = 20
+        
+        struct BBoxLimits
+        {
+            static let Width = 1.0
+            static let Height = 1.0
+            static let LatRange = (-90.0, 90.0)
+            static let LonRange = (-180.0, 180.0)
+        }
     }
 
-    struct ParameterKeys
+    struct Request
     {
-        static let Longitude = "lon"
-        static let Latitude = "lat"
-        static let Extras = "extras"
-        static let APIKey = "api_key"
-        static let Method = "method"
-        static let Format = "format"
-        static let NoJSONCallback = "nojsoncallback"
-        static let PerPage = "per_page"
-        static let Page = "page"
+        struct ParamKey
+        {
+            static let Method = "method"
+            static let APIKey = "api_key"
+            static let Extras = "extras"
+            static let BoundingBox = "bbox"
+            static let Format = "format"
+            static let NoJSONCallback = "nojsoncallback"
+            static let Page = "page"
+            static let PerPage = "per_page"
+            static let Longitude = "lon"
+            static let Latitude = "lat"
+        }
+    
+        struct ParamVal
+        {
+            static let Method = "flickr.photos.search"
+            static let APIKey = "c9e2bdb5c6d04dfd0c58742eb4215dfc"
+            static let MediumURL = "url_m"
+            static let Format = "json"
+            static let DisableJSONCallback = "1"
+            static let PhotosPerPage = "25"
+        }
     }
     
-    struct ParameterValues
+    struct Response
     {
-        static let MediumURL = "url_m"
-        static let ResponseFormat = "json"
-        static let DisableJSONCallback = "1"
-        static let PhotosPerPage = "25"
+        struct Key
+        {
+            static let Status = "stat"
+            static let PhotosDictionary = "photos"
+            static let Photo = "photo"
+            static let MediumURL = "url_m"
+            static let Title = "title"
+            static let Pages = "pages"
+            static let Total = "total"
+        }
     }
     
-    struct APIKeys
+    struct Error
     {
-        static let flickr = "c9e2bdb5c6d04dfd0c58742eb4215dfc"
-    }
-    
-    struct JSONResponseKeys
-    {
-        static let PhotosDictionary = "photos"
-        static let PhotosArray = "photo"
-        static let MediumURL = "url_m"
+        struct Domain
+        {
+            static let searchMethod="FLICKR.SEARCH_PHOTOS"
+        }
+        
+        struct Message
+        {
+            static let Error_Occurred = "Error occurrred in API response."
+            static let Invalid_Response = "API response is not parsable for necessary information."
+        }
     }
 }
